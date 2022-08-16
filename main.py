3c21cfe1b9f5ab9e9a79d0f99df621c56e7c04bb
James <mjamesnguyen@gmail.com>
2022-08-16 12:10:37 -0700

    ititializes gpio 20-22

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ def initLeds():
     for i in range(litLED):
         Pin(i).value(1)
 
+# initializes interrupts to gpios 20-22
+def initButtons():
+    p20 = Pin(20, Pin.IN, Pin.PULL_UP)
+    p20.irq(lambda pin: print("GP20 has been pressed"), Pin.IRQ_FALLING)
+
+    p21 = Pin(21, Pin.IN, Pin.PULL_UP)
+    p21.irq(lambda pin: print("GP21 has been pressed"), Pin.IRQ_FALLING)
+
+    p22 = Pin(22, Pin.IN, Pin.PULL_UP)
+    # p22.irq(lambda pin: print("GP22 has been pressed"), Pin.IRQ_FALLING)
+    p22.irq(countDown, Pin.IRQ_FALLING)
 
 
 while True:
