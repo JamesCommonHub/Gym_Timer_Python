0aa0674b00329474ef2221390d51aec76510c47b
James <mjamesnguyen@gmail.com>
2022-08-28 19:08:06 -0700

    Updated initLeds and initButtons functions

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 from machine import Pin, Timer, PWM
+import time
 
 
-# global variables
+# globals
 litLED = 5
 setTime = 5
+currentTime = 5
+onBoardLed = Pin(25, Pin.OUT)
+
 
 # timer object
 minCountdown = Timer()
 ledBlink = Timer()
+
 
 # initializes LED gpios 0-9
 def initLeds():
     
     for i in range(10):
         Pin(i, Pin.OUT)
-        # print(i)
-        
-    for i in range(litLED):
-        Pin(i).value(1)
+
 
 # initializes interrupts to gpios 20-22
 def initButtons():
     p20 = Pin(20, Pin.IN, Pin.PULL_UP)
-    p20.irq(lambda pin: print("GP20 has been pressed"), Pin.IRQ_FALLING)
+    p20.irq(addOne, Pin.IRQ_FALLING)
 
     p21 = Pin(21, Pin.IN, Pin.PULL_UP)
-    p21.irq(lambda pin: print("GP21 has been pressed"), Pin.IRQ_FALLING)
+    p21.irq(subOne, Pin.IRQ_FALLING)
 
     p22 = Pin(22, Pin.IN, Pin.PULL_UP)
-    # p22.irq(lambda pin: print("GP22 has been pressed"), Pin.IRQ_FALLING)
-    p22.irq(countDown, Pin.IRQ_FALLING)
+    p22.irq(startTimer, Pin.IRQ_FALLING)
 
+
+# running program
+
+initLeds()
+initButtons()
 
 while True:
     pass
