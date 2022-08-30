d16178cbe7a5cae063b3c28ad973972108ebe4cf
James <mjamesnguyen@gmail.com>
2022-08-29 21:27:18 -0700

    added start and countdown functionalities

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ litLED = 5
 setTime = 5
 currentTime = 5
 onBoardLed = Pin(25, Pin.OUT)
+soundPin = Pin(18)
+beepPWM = None
+beepFreq = 500
+beepDutyCycle = 32768
 
-
-# timer object
 minCountdown = Timer()
-ledBlink = Timer()
+blinkLed = Timer()
+gymTimerPeriod = 3000 # one minute = 60000
+blinkLedPeriod = 500
+
+timerState = False
 
 
 # initializes LED gpios 0-9
@@ -65,6 +71,56 @@ def subOne(pin):
         currentTime = setTime
         print("Timer set for %d minutes." % setTime)
         updateLeds()
+        
+
+# start / stop timer function when gpio 22 is pressed
+def startTimer(pin):
+    global setTime
+    global currentTime
+    global minCountdown
+    global blinkLed
+    global timerState
+    
+    if timerState != True:  
+        timerState = True
+        print("Starting timer countdown from %d minutes..." % setTime)
+        minCountdown.init(mode = Timer.PERIODIC, period = gymTimerPeriod, callback = countDown)
+        blinkLed.init(mode = Timer.PERIODIC, period = blinkLedPeriod, callback = blinkingLed)
+    else:
+        print("Timer reset...")
+        resetTimer()
+
+
+# minCountDown timer callback function, counts down from setTime / currentTime
+def countDown(pin):
+    global currentTime
+    global minCountdown
+    global blinkLed
+    
+    currentTime -= 1
+    
+    if currentTime > 0:
+        print("%d minutes left..." % currentTime)
+        updateLeds()
+    else:
+        print("Times up!!")
+        updateLeds()
+        minCountdown.deinit()
+        blinkLed.deinit()
+        beepOn()
+        time.sleep(2)
+        beepOff()
+        resetTimer()
+        
+
+# blinkLed timer callback function, blinks the onboard LED
+def blinkingLed(pin):
+    global onBoardLed
+    
+    onBoardLed.toggle()
+
+
+
 
 # running program
 
